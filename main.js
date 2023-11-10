e4d5d1dc3644a7af3279b2137bcff40e51dc9675
DavideCiammetti <davideciammetti97@gmail.com>
2023-11-10 18:20:25 +0100

    feat: mileston 1 2

--- main.js
+++ main.js
@@ -17,13 +17,51 @@
 </div> */
 
 const imgArray = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+const tastoAlto = document.querySelector('.prev');
+const tastoBasso = document.querySelector('.next');
 const items = document.querySelector('.items');
+let actualImg = 0;
 
 for(let i= 0; i < imgArray.length; i++){
 
+    // creo l'elemento html <div>
     const item = document.createElement('div');
-    item.innerHTML += `<div class="item active"><img src="./img/${imgArray[i]}" alt="img.1" /></div>`;
+    // aggiungo la classe item a div
+    item.classList.add('item')
+    // aggiungo l'immagine a div
+    item.innerHTML += `<img src="./img/${imgArray[i]}" alt="img.1" />`;
+
+    if(i === actualImg){
+        item.classList.add('active');
+    }
 
     items.append(item);
-
 }
+
+const allItem = document.querySelectorAll('.item');
+console.log(allItem);
+
+// creo evento per premere pulsante in alto
+tastoAlto.addEventListener('click', function() {
+    console.log(actualImg);
+    if(actualImg < imgArray.length - 1){
+
+        allItem[actualImg].classList.remove('active');
+        actualImg++;
+        allItem[actualImg].classList.add('active');
+    }
+
+});
+
+
+// creo evento per premere pulsante in basso
+tastoBasso.addEventListener('click', function() {
+    console.log(actualImg);
+    if(actualImg > 0 ){
+
+        allItem[actualImg].classList.remove('active');
+        actualImg--;
+        allItem[actualImg].classList.add('active');
+    }
+
+});
